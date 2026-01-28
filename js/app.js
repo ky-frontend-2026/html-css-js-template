25660791736eeaf878bc1cdde732dcf7a8921d98
lizb <loo4love90@gmail.com>
2026-01-28 11:42:24 +0900

    sec-5 update2

--- js/app.js
+++ js/app.js
@@ -45,8 +45,8 @@ fixedArrow.addEventListener("click", () => {
 });
 
 
-const sec4MenuList = document.querySelectorAll(".sec-5-menu li");
-const sec4ContentList = document.querySelectorAll(".sec-5-content-wrap .content");
+const sec5MenuList = document.querySelectorAll(".sec-5-menu li");
+const sec5ContentList = document.querySelectorAll(".sec-5-content-wrap .content");
 let crr = 0;
 
 
@@ -54,18 +54,18 @@ let crr = 0;
 init(crr);
 
 function init(x) {
- sec4MenuList[x].classList.add("active");
- sec4ContentList[x].classList.add("on");
+ sec5MenuList[x].classList.add("active");
+ sec5ContentList[x].classList.add("on");
 }
 
 function reset() {
- for (let i = 0; i < sec4MenuList.length; i++) {
-  sec4MenuList[i].classList.remove("active");
-  sec4ContentList[i].classList.remove("on");
+ for (let i = 0; i < sec5MenuList.length; i++) {
+  sec5MenuList[i].classList.remove("active");
+  sec5ContentList[i].classList.remove("on");
  }
 }
 
-sec4MenuList.forEach((menu, index) => {
+sec5MenuList.forEach((menu, index) => {
  menu.addEventListener("click", (e) => {
     e.preventDefault()
   // console.log(menu,index)
