ceeac63210f4dbec5441079d4c2d7b20588105b9
lizb <loo4love90@gmail.com>
2026-01-30 14:18:36 +0900

    final-update

--- js/app.js
+++ js/app.js
@@ -9,15 +9,14 @@ const navList = document.querySelectorAll(".nav-list li");
 // [0,1,2,3]
 
 navList.forEach((btn, i) => {
- btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(document.querySelector(`#sec-${i + 1}`));
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(document.querySelector(`#sec-${i + 1}`));
 
-
-  document.querySelector(`#sec-${i + 1}`).scrollIntoView({
-    behavior:'smooth'
-  })
- });
+    document.querySelector(`#sec-${i + 1}`).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
 
 // classList.toggle() 주체 body
@@ -25,86 +24,82 @@ navList.forEach((btn, i) => {
 // console.log(fixedArrow)
 
 mobNavBtn.addEventListener("click", (e) => {
- e.preventDefault();
- body.classList.toggle("mob-nav-open");
+  e.preventDefault();
+  body.classList.toggle("mob-nav-open");
 });
 
 window.addEventListener("scroll", () => {
- if (window.scrollY >= 100) {
-  body.classList.add("scroll");
- } else {
-  body.classList.remove("scroll");
- }
+  if (window.scrollY >= 100) {
+    body.classList.add("scroll");
+  } else {
+    body.classList.remove("scroll");
+  }
 });
 
 fixedArrow.addEventListener("click", () => {
- window.scrollTo({
-  top: 0,
-  behavior: "smooth",
- });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
 
-
 const sec5MenuList = document.querySelectorAll(".sec-5-menu li");
-const sec5ContentList = document.querySelectorAll(".sec-5-content-wrap .content");
+const sec5ContentList = document.querySelectorAll(
+  ".sec-5-content-wrap .content",
+);
 let crr = 0;
-
-
 
 init(crr);
 
 function init(x) {
- sec5MenuList[x].classList.add("active");
- sec5ContentList[x].classList.add("on");
+  sec5MenuList[x].classList.add("active");
+  sec5ContentList[x].classList.add("on");
 }
 
 function reset() {
- for (let i = 0; i < sec5MenuList.length; i++) {
-  sec5MenuList[i].classList.remove("active");
-  sec5ContentList[i].classList.remove("on");
- }
+  for (let i = 0; i < sec5MenuList.length; i++) {
+    sec5MenuList[i].classList.remove("active");
+    sec5ContentList[i].classList.remove("on");
+  }
 }
 
 sec5MenuList.forEach((menu, index) => {
- menu.addEventListener("click", (e) => {
-    e.preventDefault()
-  // console.log(menu,index)
-  reset();
-  init(index);
- });
+  menu.addEventListener("click", (e) => {
+    e.preventDefault();
+    // console.log(menu,index)
+    reset();
+    init(index);
+  });
 });
- const swiper = new Swiper(".sec-1-slider", {
-  loop:true,
-      pagination: {
-        el: ".swiper-pagination",
-      },
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+const swiper = new Swiper(".sec-1-slider", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+const bgWrap = document.querySelectorAll(".sec-3 .bg-wrap > div ");
 
-
-    const bgWrap= document.querySelectorAll('.sec-3 .bg-wrap > div ')
-
-bgWrap[0].classList.add('active')
+bgWrap[0].classList.add("active");
 // active
 
 var sec3slider = new Swiper(".sec-3-slider", {
-  loop:true,
+  loop: true,
   navigation: {
     nextEl: ".sec-3 .next",
     prevEl: ".sec-3 .prev",
   },
-  on:{
-    activeIndexChange:function(){
-        const i = this.realIndex
+  on: {
+    activeIndexChange: function () {
+      const i = this.realIndex;
 
-        console.log(`현재 index ${i}`)
-        bgWrap.forEach((bg)=>bg.classList.remove('active'))
-        bgWrap[i].classList.add('active')
-
-    }
-  }
+      console.log(`현재 index ${i}`);
+      bgWrap.forEach((bg) => bg.classList.remove("active"));
+      bgWrap[i].classList.add("active");
+    },
+  },
 });
